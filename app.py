2c595b4eda36fd38d14c38d7d23aa406b916efba
Kandasamy-S <sarathykandasamy@gmail.com>
2024-08-08 21:45:20 +0530

    CSS added

--- app.py
+++ app.py
@@ -1,27 +1,81 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
-app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
-db=SQLAlchemy(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 with app.app_context():
     db.create_all()
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
+
 class Todo(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    content=db.Column(db.String(200),nullable=False)
-    completed=db.Column(db.Integer,default=0)
-    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/',methods=['POST','GET'])
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, password=hashed_password)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful!', category='success')
+            return redirect(url_for('login'))
+        except:
+            flash('Username already exists', category='error')
+            return redirect(url_for('register'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'error')
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/', methods=['POST', 'GET'])
+@login_required
 def index():
-    if request.method=='POST':
+    if request.method == 'POST':
         task_content = request.form['content']
-        new_task=Todo(content=task_content)
+        new_task = Todo(content=task_content)
         try:
             db.session.add(new_task)
             db.session.commit()
@@ -29,10 +83,11 @@ def index():
         except:
             return "Oops Something went wrong!!!"
     else:
-        tasks=Todo.query.order_by(Todo.date_created).all()
-        return render_template("index.html",tasks=tasks)
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
 
 @app.route('/delete/<int:id>')
+@login_required
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
     try:
@@ -42,18 +97,19 @@ def delete(id):
     except:
         return "Oops Something went wrong in deleting!!!" 
 
-@app.route('/update/<int:id>',methods=['GET','POST'])
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
 def update(id):
-    task=Todo.query.get_or_404(id)
-    if request.method=='POST':
-        task.content=request.form['content']
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
         try:
-           db.session.commit()
-           return redirect('/')
+            db.session.commit()
+            return redirect('/')
         except:
             return "Oops Something went wrong in updating!!!"  
     else:    
-        return render_template("update.html",task=task)
+        return render_template("update.html", task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
